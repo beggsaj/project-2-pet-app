719f83069a0aaa0016e6dcaac2167839f9247de6
Priscilla <egli.priscilla@gmail.com>
2022-01-29 11:01:34 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
-let sequelize;
+const Sequelize = require('sequelize');
 
+require('dotenv').config();
 
 // create connection to our db
-
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
