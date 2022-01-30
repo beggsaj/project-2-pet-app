36f45578071da50f33c199c743ea54e371934b7e
Abi Beggs <beggsaj@gmail.com>
2022-01-30 16:29:43 -0600

    added image

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,103 +3,103 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'This is the best boy.',
-    post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 10
   },
   {
     title: 'Why my dogs better than yours.',
-    post_url: 'https://nasa.gov/donec.json',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 8
   },
   {
     title: 'He could be on paw patrol',
-    post_url: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 1
   },
   {
     title: 'Does any one know my dogs breed?',
-    post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 4
   },
   {
     title: 'How can I tell if my dog is pureblood?',
-    post_url: 'http://google.ca/nam/nulla/integer.aspx',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 7
   },
   {
     title: 'I caught fleas.',
-    post_url: 'https://stanford.edu/consequat.png',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 4
   },
   {
     title: 'How do I get rid of fleas?',
-    post_url: 'http://edublogs.org/non/ligula/pellentesque.js',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 1
   },
   {
     title: 'Who has the cutest breed?',
-    post_url: 'http://ucla.edu/consequat/nulla.html',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 1
   },
   {
     title: 'Is this a mutt?',
-    post_url: 'http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 9
   },
   {
     title: 'How to tell if your mutt has fleas?',
-    post_url: 'https://reverbnation.com/ligula/sit.jpg',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 5
   },
   {
     title: 'Looking for fellow breeders.',
-    post_url: 'http://china.com.cn/lectus/vestibulum.json',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 3
   },
   {
     title: 'Why do people hate my pitbull?',
-    post_url: 'http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 10
   },
   {
     title: 'Look at my dog.',
-    post_url: 'https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 8
   },
   {
     title: 'Any single doggo lovers out there?',
-    post_url: 'https://lycos.com/natoque/penatibus/et.html',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 3
   },
   {
     title: 'How to train your doggy.',
-    post_url: 'https://gmpg.org/lorem.jpg',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 3
   },
   {
     title:
       'Why does he chase his tail?',
-    post_url: 'https://paginegialle.it/mattis/egestas.jsp',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 7
   },
   {
     title: 'Fifty shades of fur a breeding guide for fellow dog breeders.',
-    post_url: 'http://wikia.com/turpis/eget.jpg',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 6
   },
   {
     title: 'Where can I find dog mints?',
-    post_url: 'https://shareasale.com/quis.json',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 4
   },
   {
     title: 'Why is my dogs breath bad',
-    post_url: 'http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 6
   },
   {
     title: 'How to get rid of fleas',
-    post_url: 'https://java.com/at/nibh/in.png',
+    post_url: '/uploads/pexels-helena-lopes-2253275.jpg',
     user_id: 7
   }
 ];
